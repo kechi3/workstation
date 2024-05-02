2033258678935678644ac6b5734a8b2cfe8fc144
Your Name <you@example.com>
2024-05-02 13:51:12 +0900

    一旦ね

--- workstation/114.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-batch = 5
-
-list = np.arange(batch)
-print(list)
-
--- workstation/step1.py
+++ workstation/step1.py
@@ -1,5 +1,27 @@
 import numpy as np
+import weakref
 import unittest
+import contextlib
+
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config,name,value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+
+def as_variable(obj):
+    if isinstance(obj, Variable):
+        return obj
+    return Variable(obj)
+
+def no_grad():
+    return using_config('enable_backprop',False)
+
+class Config:
+    enable_backprop = True
 
 class SquareTest(unittest.TestCase):
     def test_forward(self):
@@ -22,15 +44,45 @@ class SquareTest(unittest.TestCase):
         self.assertTrue(flg)
 
 class Variable:
-    def __init__(self,data):
+    __array_priority__= 200
+    def __init__(self,data, name = None):
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError('{} is not supported'.format(type(data)))
 
         self.data = data
+        self.name = name
         self.grad = None
         self.creator = None
         self.generation = 0
+
+    def __len__(self):
+        return len(self.data)
+
+    def __mul__(self, other):
+        return mul(self,other)
+
+    def __repr__(self):
+        if self.data is None:
+            return 'valuable(None)'
+        p = str(self.data).replace('\n','\n'+' '*9)
+        return 'valuable('+p+')'
+
+    @property
+    def shape(self):
+        return self.data.shape
+
+    @property
+    def ndim(self):
+        return self.data.ndim
+
+    @property
+    def size(self):
+        return self.data.size
+
+    @property
+    def dtype(self):
+        return self.data.dtype
 
     def cleargrad(seld):
         self.grad = None
@@ -39,13 +91,21 @@ class Variable:
         self.creator = func
         self.generation = func.generation + 1
 
-    def backward(self):
+    def backward(self, retain_grad=False):
         if self.grad is  None:
             self.grad = np.ones_like(self.data)
-        funcs = [self.creator]
+        funcs = []
+        seen_set = set()
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x:x.generation)
+        add_func(self.creator)
+
         while funcs:
             f = funcs.pop()
-            gys = [output.grad for output in f.outputs]
+            gys = [output().grad for output in f.outputs]
             gxs = f.backward(*gys)
             if not isinstance(gxs, tuple):
                 gxs = (gxs,)
@@ -55,20 +115,25 @@ class Variable:
                 else:
                     x.grad = x.grad + gx
                 if x.creator is not None:
-                    funcs.append(x.creator)
+                    add_func(x.creator)
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None
 
 class Function:
     def __call__(self,*inputs):
+        inputs = [as_variable(x) for x in inputs]
         xs = [x.data for x in inputs]
         ys = self.forward(*xs)
         if not isinstance(ys,tuple):
             ys = (ys,)
         outputs = [Variable(as_array(y)) for y in ys]
-        self.generation = max([x.generation for x in inputs])
-        for output in outputs:
-            output.set_creator(self)
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs])
+            for output in outputs:
+                output.set_creator(self)
         self.inputs = inputs
-        self.outputs = outputs
+        self.outputs = [weakref.ref(output) for output in outputs]
         return outputs if len(outputs)>1 else outputs[0]
 
     def forward(self, x):
@@ -100,6 +165,50 @@ class Exp(Function):
         x = self.inputs[0].data
         return np.exp(x)*gy
 
+class Neq(Function):
+    def forward(self,x):
+        return -x
+    def backward(self,gy):
+        return -gy
+
+class Mul(Function):
+    def forward(self,x0,x1):
+        y = x0*x1
+        return y
+    def backward(self,gy):
+        x0,x1 = self.inputs[0].data,self.inputs[1].data
+        return gy*x1,gy*x0
+
+class Sub(Function):
+    def forward(self,x0,x1):
+        y = x0- x1
+        return y
+    def backward(self,gy):
+        return gy,-gy
+
+class Div(Function):
+    def forward(self,x0,x1):
+        y = x0 / x1
+        return y
+    def backward(self,gy):
+        x0,x1 = self.inputs[0].data,self.inputs[0].data
+        gx0 = gy /x1
+        gx1 = gy*(-x0 /x1**2)
+        return gy0,gy1
+
+class Pow(Function):
+    def __init__(self,c):
+        self.c = c
+
+    def forward(self,x):
+        y = x**self.c
+        return y
+    def backward(self,gy):
+        x = self.inputs[0].data
+        c = self.c
+        gx = c*x**(c-1)*gy
+        return gx
+
 def as_array(x):
     if np.isscalar(x):
         return np.array(x)
@@ -121,10 +230,48 @@ def square(x):
     return f(x)
 
 def add(x0, x1):
+    x1 = as_array(x1)
     return Add()(x0, x1)
 
 def exp(x):
     return Exp()(x)
+
+def neq(x):
+    return Neq()(x)
+
+def mul(x0,x1):
+    x1 = as_array(x1)
+    return Mul()(x0,x1)
+
+def sub(x0,x1):
+    x1 = as_array(x1)
+    return Sub()(x0,x1)
+
+def rsub(x0,x1):
+    x1 = as_array(x1)
+    return Sub()(x1,x0)
+
+def div(x0,x1):
+    x1 = as_array(x1)
+    return Div()(x0,x1)
+
+def rdiv(x0,x1):
+    x1 = as_array(x1)
+    return Div()(x1,x0)
+
+def pow(x,c):
+    return Pow(c)(x)
+
+Variable.__add__ = add
+Variable.__radd__ = add
+Variable.__mul__ = mul
+Variable.__rmul__ = mul
+Variable.__neq__ = neq
+Variable.__sub__ = sub
+Variable.__rsub__ = rsub
+Variable.__truediv__ = div
+Variable.__rtruediv__ = rdiv
+Variable.__pow__ = pow
 
 x = Variable(np.array(0.5))
 
@@ -135,9 +282,7 @@ y = square(b)
 y.backward()
 print(x.grad)
 
-x = Variable(np.array(3.0))
-y = add(add(x,x),x)
-y.backward()
-print(y.data)
-print(x.grad)
+x = Variable(np.array(2.0))
+y = x**3
+print(y)
 
